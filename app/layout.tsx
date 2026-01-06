f142328429bc79715a3bf30673c39da6e6136cb1
iranspro <00604p@gmail.com>
2026-01-06 18:41:01 +0330

    fix: add Suspense for Spline

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import SplineBackground from "@/components/layout/spline-background";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Irans Pro - اینترنت آزاد برای ایران",
@@ -18,12 +19,14 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" className={YekanBakh.variable}>
       <body className="antialiased bg-[#0a0f1a] text-white font-yekan">
-        <SplineBackground />
         <div className="relative z-10 max-w-2xl mx-auto px-4 py-4 md:py-8 flex flex-col items-center">
           <Header />
           <main className="w-full">{children}</main>
           <Footer />
         </div>
+        <Suspense>
+          <SplineBackground />
+        </Suspense>
       </body>
     </html>
   );
