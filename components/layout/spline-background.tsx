471c10bab41feda4174c0e5342c03fb95adbd3cb
iranspro <00604p@gmail.com>
2026-01-06 15:07:53 +0330

    fix: spline url

--- components/layout/spline-background.tsx
+++ components/layout/spline-background.tsx
@@ -22,7 +22,7 @@ export default function SplineBackground() {
     <div className="fixed inset-0 overflow-hidden pointer-events-none -z-10">
       <Suspense fallback={<div className="bg-[#0a0f1a] w-full h-full" />}>
         {mount && (
-          <Spline scene="https://prod.spline.design/3JH1YJX1bXoX4YpS/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/DmoWFKEKpfDgzqay/scene.splinecode" />
         )}
       </Suspense>
       <div className="absolute inset-0 bg-[#0a0f1a]/90" />
